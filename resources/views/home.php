40dc8ae8d3da9f1c6ab72a96cc90c7a46b608074
Menna Medhat <mnnmdht@gmail.com>
2020-04-05 22:26:37 +0200

    init theme

---
+++ resources/views/home.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  <title>Premast Studio</title>
+
+</head>
+<body>
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-light ">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <a class="navbar-brand" href="#">
+          <img src="studio.png" alt="">
+        </a>
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Portfolio<span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Contact Us</a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <button id ="rq" class="btn btn-outline-success my-2 my-sm-0" type="submit">request your presentation</button>
+        </form>
+      </div>
+    </nav>
+  </header>
+
+  <section style="background-image: url('home.png'); background-size: cover; background-repeat: no-repeat; background: linear-gradient(105.63deg, rgba(20, 93, 233, 0.85) -15.92%, rgba(160, 74, 249, 0.85) 101.46%), linear-gradient(180deg, rgba(65, 65, 65, 0.26) 0%, rgba(12, 12, 12, 0.65) 29.52%, rgba(29, 29, 29, 0.72) 62.12%, rgba(49, 49, 49, 0.44) 100%),; background-blend-mode: soft-light, normal, normal; border-radius: 20px; width: 1370px;
+  height: 586px; margin:0 36px 0 36px">
+   <div class="container">
+     <div class="row justify-content-center">
+       <div class="center_text">
+         <h1 class="heading">We Create Neat, Usable & Amazing <br>presentations</h1>
+         <h5 class="sub_heading"> Hire our most talented design team to design your next presentation and make it <br> mindblowing! or you can get custom branded fully editable templates, you can just change <br> the color, size or even use different graphics from our platform.</h5>
+       </div>
+     </div>
+
+     <div class="row justify-content-center">
+       <ul class="icons">
+         <li>
+           <img src="pp.png">
+         </li>
+         <li>
+           <img src="pr.png">
+         </li>
+         <li>
+           <img src="gg.png" alt="">
+         </li>
+       </ul>
+     </div>
+   </div>
+  </section>
+
+  <section>
+    <div class="container">
+      <div class="row justify-content-center">
+        <h5 class="ex">Examples of our work</h5>
+      </div>
+      <div class="row justify-content-center">
+        <h2>Little change can be impactful</h2>
+      </div>
+      <div class="portfolio">
+      <div class="row">
+          <div class="col-md-6">
+            <h4>Before Premast</h4>
+            <img src="slide.png" alt="">
+          </div>
+
+          <div class="col-md-6">
+            <h4 class="purple">After Premast</h4>
+            <img src="slide.png" >
+          </div>
+
+          <div class="col-md-12" >
+            <div class="full">
+              <a  class="btn btn-outline-secondary" href="#">See Full Portfolio</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+  </section>
+
+  <section style="background: #282F39; border-radius: 20px; margin:30px;">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="serv">
+          <h5>Our Servcies</h5>
+          <h2 class="serv-sub">Amazing templates, Ready for your next <br> presentation</h2>
+
+        </div>
+
+      </div>
+      <div class="row">
+        <div class="col-md-5">
+          <img src="Group.png" >
+          <div class="top-left-text">
+            <p style="padding-left:16px;">Refining</p>
+          </div>
+          <div class="servics">
+            <h4 class="design"> Redesign</h4>
+            <h4 class="design"> Redraw</h4>
+            <h4 class="design"> Master Template</h4>
+
+          </div>
+        </div>
+        <div class="col-md-2">
+          <div class="file" style="padding-bottom: 60px;">
+            <img src="file.png">
+          </div>
+          <div class="smart">
+            <img src="sm.png" style="padding-bottom: 60px;">
+
+          </div>
+          <div class="paint">
+            <img src="paint.png" style="padding-bottom: 60px;">
+
+          </div>
+
+        </div>
+
+        <div class="col-md-5">
+          <div class="gg">
+            <h4 class="pres">Set your presentation</h4>
+            <p class="sub_press">Your original presentation slides with your content, fonts and designs</p>
+
+            <h4 class="pres">Fix it with retouches</h4>
+            <p class="sub_press">Using your original presentation including all the element we change it a little bit to look nicer and more professional </p>
+
+            <h4 class="pres">Voila!</h4>
+            <p class="sub_press">We will do our best to have your perfect presentation and deliver satisfaction</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section>
+    <div class="container">
+      <div class="row justify-content-center">
+        <h5 class="clients">Our Clients</h5>
+        <h2 class="clients"></h2>
+
+      </div>
+
+    </div>
+  </section>
+
+  <section style="background-image: url(bg.png); background: linear-gradient(0deg, rgba(40, 47, 57, 0.93), rgba(40, 47, 57, 0.93)),; background-repeat: no-repeat;border-radius: 20px; background-position: center; background-size:cover;">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="headings">
+          <h2 class="first">Ready to get started?</h2>
+         <!-- messing content here  -->   <p class="first">Labore nisi laborum ut non cupidatat irure sit elit in reprehenderit veniam laborum et et. Labore mollit aliqua et sunt dolore cillum.</p>
+
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+
+</body>
+</html>
